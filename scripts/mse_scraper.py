98716e2594de530640c05806b03a5fed29731a45
puje22 <purevjav.tmn@gmail.com>
2026-09-14 20:56:35 +0800

    success

--- scripts/mse_scraper.py
+++ scripts/mse_scraper.py
@@ -51,7 +51,7 @@ TARGET_TICKERS = {
     "MFG", "XOC",
 }
 
-BASE_URL = "https://old.mse.mn/en/company/{id}"
+BASE_URL = "https://members.mse.mn/en/company/{id}"
 ID_RANGE = range(1, 3000)          # widen if some tickers aren't found
 REQUEST_DELAY = 0.3               # seconds between requests (politeness)
 MAX_WORKERS = 6                   # parallel requests; lower if you get blocked
